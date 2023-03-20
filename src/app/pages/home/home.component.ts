9d98b8cac78fbc7e741768d8e7e743507b097d56
sebastian-VB <sebas99peru@gmail.com>
2023-03-20 13:13:25 -0600

    added navigation between pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  constructor(private router: Router){}
+
+  goToPageAnimes():void{
+    // console.log('Ir a animes');
+    this.router.navigate(['/animes']);
+  }
+  
+  goToPageMangas():void{
+    // console.log('Ir a mangas');
+    this.router.navigate(['/mangas']);
+  }
 
 }
