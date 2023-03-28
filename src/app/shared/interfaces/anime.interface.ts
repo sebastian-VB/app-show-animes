6983db0f3d66f75c1997ad8445561654e865003c
sebastian-VB <sebas99peru@gmail.com>
2023-03-28 11:00:52 -0600

    was added anime interface

--- src/app/shared/interfaces/anime.interface.ts
+++ src/app/shared/interfaces/anime.interface.ts
@@ -1,4 +1,136 @@
+import { Genre } from "./genre.interface"
 
-export interface Anime{
-    
+export interface Anime {
+    mal_id: number
+    url: string
+    images: Images
+    //trailer: Trailer
+    //approved: boolean
+    //titles: Title[]
+    title: string
+    title_english: string
+    title_japanese: string
+    title_synonyms: string[]
+    //type: string
+    //source: string
+    episodes: number
+    //status: string
+    //airing: boolean
+    //aired: Aired
+    duration: string
+    rating: string
+    score: number
+    scored_by: number
+    rank: number
+    popularity: number
+    //members: number
+    favorites: number
+    synopsis: string
+    background: string
+    season: string
+    year: number
+    //broadcast: Broadcast
+    //producers: Producer[]
+    //licensors: Licensor[]
+    studios: Studio[]
+    genres: Genre[]
+    //explicit_genres: any[]
+    themes: Theme[]
+    //demographics: any[]
+  }
+  
+interface Images {
+    jpg: Jpg
+    webp: Webp
+}
+
+interface Jpg {
+    image_url: string
+    small_image_url: string
+    large_image_url: string
+}
+
+interface Webp {
+    image_url: string
+    small_image_url: string
+    large_image_url: string
+}
+
+// interface Trailer {
+//     youtube_id: string
+//     url: string
+//     embed_url: string
+//     images: Images2
+// }
+
+// export interface Images2 {
+// image_url: string
+// small_image_url: string
+// medium_image_url: string
+// large_image_url: string
+// maximum_image_url: string
+// }
+
+// export interface Title {
+// type: string
+// title: string
+// }
+
+// export interface Aired {
+// from: string
+// to: string
+// prop: Prop
+// string: string
+// }
+
+// export interface Prop {
+// from: From
+// to: To
+// }
+
+interface From {
+    day: number
+    month: number
+    year: number
+}
+
+interface To {
+    day: number
+    month: number
+    year: number
+}
+
+// export interface Broadcast {
+// day: string
+// time: string
+// timezone: string
+// string: string
+// }
+
+// export interface Producer {
+// mal_id: number
+// type: string
+// name: string
+// url: string
+// }
+
+// export interface Licensor {
+// mal_id: number
+// type: string
+// name: string
+// url: string
+// }
+
+interface Studio {
+    mal_id: number
+    type: string
+    name: string
+    url: string
+}
+
+interface Theme {
+    mal_id: number
+    type: string
+    name: string
+    url: string
 }
--- src/app/shared/services/searchAnime.service.ts
+++ src/app/shared/services/searchAnime.service.ts
@@ -2,6 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
 import { environment } from 'src/environment/environment';
+import { Anime } from '../interfaces/anime.interface';
+
+interface ResponseData {
+    data: Anime[]
+}
 
 @Injectable({
     providedIn: 'root'
@@ -10,10 +15,10 @@ export class SearchAnimeService{
 
     constructor(private http: HttpClient){}
 
-    getAnime(titleAnime: string): Observable<any>{
+    getAnime(titleAnime: string): Observable<Anime[]>{
         const params = { q: titleAnime }
 
-        return this.http.get<any>(`${environment.apiUrl}anime`, {params}).pipe(
+        return this.http.get<ResponseData>(`${environment.apiUrl}anime`, {params}).pipe(
             map(res => res.data)
         );
     }
