e72a56de3ca10276a6bdd1d2dde36873addaec7c
sebastian-VB <sebas99peru@gmail.com>
2023-04-05 13:16:18 -0600

    added logic in the pagination

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -8,15 +8,22 @@ import { Component } from '@angular/core';
 export class PaginationComponent {
 
   pagesNumber: Array<number> = [1,2,3,4,5];
+  totalPageNumber: Array<number> = this.pagesNumber;
 
   //mostrar 5 paginas siguientes
   nextPages(): void{
-    console.log('siguiente');
+    let last = this.totalPageNumber[this.totalPageNumber.length - 1];
+    this.pagesNumber.splice(0, last);
+    for (let index = last + 1; index <= last + 5; index++) { this.pagesNumber.push(index); }
   }
   
   //mostrar 5 paginas posteriores
   previousPages(): void{
-    console.log('anterior');
+    let first = this.totalPageNumber[0];
+    if(first > 1){
+      this.pagesNumber.splice(0, 5);
+      for (let index = first - 5 ; index < first; index++) { this.pagesNumber.push(index); }
+    }
   }
 
   //emitir el valor (pagina) seleccionado
